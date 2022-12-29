647ee53e9f58fcb2b53641d602a39e9785620c02
Manoel Duran <manoel.duran@hotmail.com>
2022-12-29 12:50:29 -0300

    feat(Js/runtime): other js runtime example

--- js/runtime/index.js
+++ js/runtime/index.js
@@ -1,11 +1,34 @@
+// JS RUNTIME
+// SIMPLE EXAMPLE
 // sync
 console.log('1');
 
 // web api -- async - the callback queue do event loop envia o callback function ao call stack da engine quando ela está limpa para executar.
-setTimeout(() => {
+setTimeout(() =>
     console.log('2')
-        , 1000
-});
+    , 1000
+);
 
 // sync
 console.log('3');
+
+
+// EXAMPLE 2
+
+function achieveNirvana() {
+    console.log('You are enlightened!')
+}
+
+function c() {
+    setTimeout(achieveNirvana, 3000)
+}
+
+function b() {
+    c()
+}
+
+function a() {
+    b()
+}
+
+a()
