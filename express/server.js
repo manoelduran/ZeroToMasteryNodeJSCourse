5be751fd4afcd645eb5c81669eccf51664f64a90
Manoel Duran <manoel.duran@hotmail.com>
2023-01-11 11:15:49 -0300

    applyed template engine to default route, applyed simple middleware to get url time, applyed routes to server

--- express/server.js
+++ express/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { friendsROuter } from './routes/friends.routes';
-import { messageRouter } from './routes/messages.routes';
-
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { friendsROuter } from '../express/routes/friends.routes.js'
+import { messageRouter } from '../express/routes/messages.routes.js'
 const app = express();
-
+const __dirname = dirname(fileURLToPath(import.meta.url))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
 const PORT = 4000;
 
 //middleware
@@ -14,10 +17,16 @@ app.use((req, res, next) => {
     const delta = Date.now() - start;
     console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 })
-
+app.use('/site', express.static(path.join(__dirname, 'public')))
 app.use(express.json());
 
-
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'My Friends Are Very Clever',
+        caption: 'Lets go Skiing',
+        image: 'site/images/skimountain.jpg'
+    })
+})
 app.use('/friends', friendsROuter);
 app.use('/messages', messageRouter);
 
