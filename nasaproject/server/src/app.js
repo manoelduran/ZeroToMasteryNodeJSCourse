6a8479494017487d455b8f26c7dcc8b5704e9e3d
Manoel Duran <manoel.duran@hotmail.com>
2023-01-19 20:54:59 -0300

    feat(package.json): created global scripts

--- nasaproject/server/src/app.js
+++ nasaproject/server/src/app.js
@@ -1,14 +1,20 @@
+import path from 'path';
 import express from 'express';
 import cors from 'cors';
 import { planetsRouter } from './routes/planets/planets.routes.js';
+import morgan from 'morgan';
 
 const app = express();
 app.use(cors({
     origin: 'http://localhost:3000',
 }));
+app.use(morgan('combined'))
+const buildDirectory = path.join('public')
 app.use(express.json());
-
+app.use(express.static(buildDirectory))
 app.use(planetsRouter);
-
+app.get('/', (req, res) => {
+    res.sendFile('public', 'index.html')
+})
 
 export { app };
