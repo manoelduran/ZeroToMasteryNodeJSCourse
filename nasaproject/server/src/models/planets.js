3d63e30231895f936b880d3809ad1e6768963dd5
Manoel Duran <manoel.duran@hotmail.com>
2023-01-19 18:27:25 -0300

    feat(back): created planets model

--- nasaproject/server/src/models/planets.js
+++ nasaproject/server/src/models/planets.js
@@ -1,3 +1,36 @@
-const planets = [];
+import { parse } from 'csv-parse';
+import fs from 'fs';
+import path from 'path';
 
-export { planets };
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED'
+        && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6;
+}
+const directory = path.join('data', 'kepler_data.csv')
+function loadPlanetsData() {
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(directory)
+            .pipe(parse({
+                comment: '#',
+                columns: true,
+            }))
+            .on('data', (data) => {
+                if (isHabitablePlanet(data)) {
+                    habitablePlanets.push(data);
+                }
+            })
+            .on('error', (err) => {
+                console.log('error', err);
+                reject(err)
+            })
+            .on('end', () => {
+                console.log(`${habitablePlanets.length} habitable planets found!`);
+                resolve()
+            });
+    })
+}
+
+export { habitablePlanets, loadPlanetsData };
--- nasaproject/server/src/routes/planets/planetsController.js
+++ nasaproject/server/src/routes/planets/planetsController.js
@@ -1,7 +1,7 @@
-import { planets } from "../../models/planets.js";
+import { habitablePlanets } from "../../models/planets.js";
 
 function getAllPlanets(req, res) {
-    return res.status(200).json(planets)
+    return res.status(200).json(habitablePlanets)
 };
 
 export { getAllPlanets };
