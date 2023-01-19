6bc9951e16c34df299a5c1174cafe06466c48fb8
Manoel Duran <manoel.duran@hotmail.com>
2023-01-19 18:28:28 -0300

    feat(server): created async function for start server after then loadPlanetsData

--- nasaproject/server/src/server.js
+++ nasaproject/server/src/server.js
@@ -1,11 +1,15 @@
 import http from 'http';
-import {app} from './app.js';
+import { app } from './app.js';
+import { loadPlanetsData } from './models/planets.js';
 
 const server = http.createServer(app);
 
-const PORT =  process.env.port || 8000;
+const PORT = process.env.port || 8000;
 
-
-server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`)
-});
+async function startServer() {
+    await loadPlanetsData()
+    server.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}...`)
+    });
+}
+startServer()
