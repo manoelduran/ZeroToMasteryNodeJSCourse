5a76edccb610462f430c8bba19249e24b38005cf
Manoel Duran <manoel.duran@hotmail.com>
2023-01-18 22:33:46 -0300

    feat(back): added cors to server and configure

--- nasaproject/server/src/app.js
+++ nasaproject/server/src/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import { planetsRouter } from './routes/planets/planets.routes.js';
 
 const app = express();
-
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
 app.use(express.json());
 
 app.use(planetsRouter);
