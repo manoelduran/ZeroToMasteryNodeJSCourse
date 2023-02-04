3a86e86caf3df1acc9c3c8c04360ae26e33aae3f
Manoel Duran <manoel.duran@hotmail.com>
2023-02-04 20:33:06 -0300

    feat(LaunchesController): created method to abort a launch

--- nasaproject/server/src/routes/launches/launchesController.js
+++ nasaproject/server/src/routes/launches/launchesController.js
@@ -1,22 +1,34 @@
-import { launches, getAllLaunches, addNewLaunch } from "../../models/launches.js";
+import { launches, getAllLaunches, addNewLaunch, deleteLauch, existsLounchWithFlightNumber } from "../../models/launches.js";
 
 function getLaunches(req, res) {
     return res.status(200).json(getAllLaunches())
 };
 
-function createLaunch(req, res) {
-    const data = req.body
-    if(!data.mission || !data.rocket || !data.launchDate || !data.target) {
-        return res.status(400).json({error: 'Missing required launch property'})
+function deleteLaunch(req, res) {
+    const flightNumber = Number(req.params.flightNumber);
+    console.log('params', flightNumber)
+    if (!existsLounchWithFlightNumber(flightNumber)) {
+        return res.status(404).json({ error: 'Launch does not exists' })
     }
-    if(isNaN(data.launchDate)) { // se a data for valida, ele retorna false e a data retorna em data.valueOf() um numero, se não for uma data valida, ele da true e retorna um NaN
+    const aborted = deleteLauch(flightNumber)
+    return res.status(200).json(aborted)
+}
+
+function createLaunch(req, res) {
+    const launch = req.body;
+    console.log('launch', launch)
+    if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.target) {
+        return res.status(400).json({ error: 'Missing required launch property' })
+    }
+    launch.launchDate = new Date(launch.launchDate)
+    if (isNaN(launch.launchDate)) { // se a data for valida, ele retorna false e a data retorna em data.valueOf() um numero, se não for uma data valida, ele da true e retorna um NaN
         return res.status(400).json({
             error: 'Invalid launch date',
         })
     }
-    data.launchDate = new Date(data.launchDate)
-    addNewLaunch(data)
-    return res.status(201).json(data);
+
+    addNewLaunch(launch)
+    return res.status(201).json(launch);
 }
 
-export { getLaunches, createLaunch };
+export { getLaunches, createLaunch, deleteLaunch };
