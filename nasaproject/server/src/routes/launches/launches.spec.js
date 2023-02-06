ac489fe6ca036034837bced1320735937f9bc760
Manoel Duran <manoel.duran@hotmail.com>
2023-02-05 21:49:34 -0300

    feat(launches.spec): created launches api unit tests

---
+++ nasaproject/server/src/routes/launches/launches.spec.js
@@ -0,0 +1,18 @@
+describe('Test GET /launches', () => {
+    it('Should be able to get all launches', () => {
+        const response = 200;
+        expect(response).toBe(200)
+    })
+})
+
+describe('Test POST /launches', () => {
+    it('Sould be able to create a launch', ()=> {
+
+    })
+    it('Sould not be able to create a launch', () => {
+
+    })
+    it('Sould catch invalid dates', () => {
+
+    })
+})
